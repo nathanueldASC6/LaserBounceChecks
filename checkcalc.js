eb6453ff86c127e6091feee089a4fb7a2430bfec
nathanueldASC6 <nathan0322@hotmail.com>
2019-10-15 23:49:50 -0400

    Finished Index

--- checkcalc.js
+++ checkcalc.js
@@ -1,11 +1,16 @@
-let hours = (process.argv[2])
-let pay = hours * 15 * .99575424 * .93798734 * .98550725 * .98950806 * .99755052 * .99848949
-console.log("$"+(Math.round(pay * 100) / 100))
+let hours = document.getElementById("hoursworked")
+const hoursSubmit = document.getElementById("submitButton")
+const hoursDisp = document.getElementById('breadgotten')
+hoursSubmit.addEventListener('click',BreadDeterminer)
+const paystubs = document.createElement("p")
+const breaded = document.createElement("h1")
 
-function BreadDeterminer(){
-if (pay >= 200){
-    return "Oh boy you making some money this week"
-} else {
-    return "You bout to be broke smh"
-}}
-console.log(BreadDeterminer());
+function BreadDeterminer(event){
+    event.preventDefault();
+    hoursDisp.innerHTML = ""
+    let userSearch = hours.value
+    console.log(userSearch)
+    let pay = userSearch * 15 * .99575424 * .93798734 * .98550725 * .98950806 * .99755052 * .99848949
+    paystubs.innerHTML = "$"+(Math.round(pay * 100) / 100)
+    hoursDisp.appendChild(paystubs)
+}
